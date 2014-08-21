7adc4c5c58536bbd3c766dc47054727775d0cc20
philippe-patenaude <philippe.patenaude@maine.edu>
2014-08-21 16:27:24 -0400

    Added a line to write to the error stream.

--- GitExample/src/example/Main.java
+++ GitExample/src/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "This is an example message!");
+		System.err.println("This is an error message.");
 	}
 
 }
