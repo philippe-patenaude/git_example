db0d30fbaecc9ad682bde97521386ec342a13cd8
philippe-patenaude <philippe.patenaude@maine.edu>
2014-08-21 16:01:46 -0400

    Initial commit

---
+++ GitExample/src/example/Main.java
@@ -0,0 +1,11 @@
+package example;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "This is an example message!");
+	}
+
+}
