c5e7e68e0d374682ed7defdc97c2a5f5100907dc
philippe-patenaude <philippe.patenaude@maine.edu>
2014-08-22 09:30:50 -0400

    Added a Hello World message to standard out.

--- GitExample/src/example/Main.java
+++ GitExample/src/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "This is an example message!");
 		System.err.println("This is an error message.");
+		System.out.println("Hello World!");
 	}
 
 }
