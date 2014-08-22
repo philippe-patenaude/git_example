6e50202d2f1e7d2bc4173e53add31b3bb8a8e5e9
philippe-patenaude <philippe.patenaude@maine.edu>
2014-08-22 14:02:09 -0400

    Added comments to the code.

--- GitExample/src/example/Main.java
+++ GitExample/src/example/Main.java
@@ -5,10 +5,15 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		// message box
 		JOptionPane.showMessageDialog(null, "This is an example message!");
+		
+		// console logs
 		System.err.println("This is an error message.");
 		System.out.println("Hello World!");
 		System.out.println("Output message!");
+		
 	}
 
 }
